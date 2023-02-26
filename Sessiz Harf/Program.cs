c0b5c9350c4b0bb783233e321dc58bc2960bb07b
cemkndz <120122784+ckkurs@users.noreply.github.com>
2023-02-26 20:58:38 +0300

    Sessiz_Harf_Patika

---
+++ Sessiz Harf/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sessiz_Harf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin girin: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+            string consonants = "bcdfghjklmnpqrstvwxyz";
+
+            foreach (string word in words)
+            {
+                bool hasConsecutiveConsonants = false;
+
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    if (consonants.Contains (word[i]) && consonants.Contains(word[i + 1]))
+                    {
+                        hasConsecutiveConsonants = true;
+                        break;
+                    }
+                }
+
+                Console.Write(hasConsecutiveConsonants + " ");
+            }
+        }
+    }
+}
